655f333c25b9829a61db023cc0b5263b6e479b53
shawyunz <shawyunz@gmail.com>
2023-09-08 23:27:24 +1200

    v1. Run the result

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			List<int> fullNumbers = new();
+			List<int> parenthesesNumbers = new List<int>()
+			{
+				11, 14, 15, 22, 47, 48, 50
+			};
+
+			for (int i = 1; i < 51; i++)
+			{
+				fullNumbers.Add(i);
+			}
+
+			Console.WriteLine("Output:");
+			foreach (var item in fullNumbers)
+			{
+				//Ignore the numbers 31 to 40
+				if (item < 41 && item > 30)
+				{
+					continue;
+				}
+
+				var outputstr = item.ToString();
+
+				//41 to 50 should have a “*” character displayed before the num
+				if (item >= 41 && item <= 50)
+				{
+					outputstr = "*" + outputstr;
+				}
+
+				//bool CheckParentheses(item);
+
+				//parentheses needed
+				if (parenthesesNumbers.Any(x => x == item))
+				{
+					outputstr = $"({outputstr})";
+				}
+
+				Console.WriteLine(outputstr);
+			}
+		}
+	}
+}

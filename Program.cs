91370d39e5443fbddf3bc5c33fada98d22b7ce5d
shawyunz <shawyunz@gmail.com>
2023-09-09 00:17:59 +1200

    v1.1 Simplify the logic. (Any to Contains)

--- Program.cs
+++ Program.cs
@@ -8,38 +8,35 @@ namespace ConsoleApp1
 	{
 		private static void Main(string[] args)
 		{
-			List<int> fullNumbers = new();
-			List<int> parenthesesNumbers = new List<int>()
+			List<int> parenthesesNumbers = new()
 			{
-				11, 14, 15, 22, 47, 48, 50
+				11,
+				14,
+				15,
+				22,
+				47,
+				48,
+				50
 			};
 
-			for (int i = 1; i < 51; i++)
-			{
-				fullNumbers.Add(i);
-			}
-
-			Console.WriteLine("Output:");
-			foreach (var item in fullNumbers)
+			for (int i = 1; i <= 50; i++)
 			{
 				//Ignore the numbers 31 to 40
-				if (item < 41 && item > 30)
+				if (i >= 30 && i <= 40)
 				{
 					continue;
 				}
 
-				var outputstr = item.ToString();
+				var outputstr = i.ToString();
 
 				//41 to 50 should have a “*” character displayed before the num
-				if (item >= 41 && item <= 50)
+				if (i >= 41 && i <= 50)
 				{
-					outputstr = "*" + outputstr;
+					outputstr = $"*{outputstr}";
 				}
 
-				//bool CheckParentheses(item);
-
 				//parentheses needed
-				if (parenthesesNumbers.Any(x => x == item))
+				if (parenthesesNumbers.Any(x => x == i))
 				{
 					outputstr = $"({outputstr})";
 				}
